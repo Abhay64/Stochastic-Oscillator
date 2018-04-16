1cae05c3d48bbbce18d033289647e0efd39b2571
AbhayKatiyar <katiyarabhay667@gmail.com>
2018-04-16 16:27:05 +0530

    Update Stochastic_indicator.py

--- Stochastic_indicator.py
+++ Stochastic_indicator.py
@@ -1,8 +1,7 @@
 import pandas as pd
 import numpy as np
 from matplotlib import pyplot as plt
-Url_polo = 'http://34.210.192.138:8000/public?keys=command,currencyPair,start,end,period&values=returnChartData,BTC_BCH,1515819652,1515841252,300'
-df = pd.read_json(Url_polo)
+df = pd.read_csv('Put your csv file here!!!')
 df['date'] = pd.to_datetime(df['date'],unit='s').dt.date
 array_date = np.array(df['date'])
 array_close = np.array(df['close'])
